a87786994a0c83c3ffc17505f18e8c16e2da27a7
tnt2ultra <46776367+tnt2ultra@users.noreply.github.com>
2020-04-06 18:06:50 +0200

    fix export to Excel

--- supplier-app/src/main/java/ru/cetelem/supplier/ui/view/CarListView.java
+++ supplier-app/src/main/java/ru/cetelem/supplier/ui/view/CarListView.java
@@ -1,23 +1,11 @@
 package ru.cetelem.supplier.ui.view;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.text.NumberFormat;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
@@ -25,7 +13,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.ResourceUtils;
 import org.vaadin.crudui.crud.impl.GridCrud;
 
 import com.vaadin.flow.component.ClickEvent;
@@ -49,26 +36,14 @@ import com.vaadin.flow.data.provider.QuerySortOrder;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
-import com.vaadin.flow.server.StreamResource;
 
-import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperCompileManager;
-import net.sf.jasperreports.engine.JasperFillManager;
-import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
-import net.sf.jasperreports.engine.export.JRXlsExporter;
-import net.sf.jasperreports.export.SimpleExporterInput;
-import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
-import net.sf.jasperreports.export.SimpleXlsExporterConfiguration;
-import net.sf.jasperreports.export.SimpleXlsReportConfiguration;
 import ru.cetelem.cassiope.supplier.model.Car;
 import ru.cetelem.cassiope.supplier.util.DateUtils;
 import ru.cetelem.supplier.service.CarService;
 import ru.cetelem.supplier.service.DictionaryService;
 import ru.cetelem.supplier.ui.component.XFGridCrud;
-import ru.cetelem.supplier.util.CarExcel;
 import ru.cetelem.supplier.util.ExcelReader;
+import ru.cetelem.supplier.util.ExportXls;
 
 @PageTitle("Car list")
 @Route(value = "", layout = MainLayout.class)
@@ -105,11 +80,15 @@ public class CarListView extends BaseView implements RouterLayout {
 		btnUpload.setIcon(new Icon(VaadinIcon.UPLOAD_ALT));
 		btnUpload.setId("btnUpload");
 		btnUpload.getElement().setAttribute("title", "Show upload XLS file dialog");
-
+		
+		ExportXls exportXls = new ExportXls(this);
 		download = new Anchor();
-		download.setHref(getCarResource());
+		download.setHref(exportXls.getCarResource());
 		download.getElement().setAttribute("title", "Download XLS file");
 		download.add(new Button(new Icon(VaadinIcon.DOWNLOAD_ALT)));
+		download.getElement().setAttribute("style", "text-decoration: none");
+		Button btnDownload = (Button) download.getChildren().findFirst().get();
+		btnDownload.setHeight("100%");
 
 		HorizontalLayout horizontalLayout = (HorizontalLayout) crudGrid
 				.getAddButton().getParent().get();
@@ -184,66 +163,6 @@ public class CarListView extends BaseView implements RouterLayout {
 			dialog.open();
 			return;
 		});
-	}
-
-	private StreamResource getCarResource() {
-		String fileName = "cars-" + LocalDate.now().format(DateTimeFormatter.ofPattern("yyMMdd")) +
-			    LocalTime.now().format(DateTimeFormatter.ofPattern("-HHmmss")) + ".xls";
-		return new StreamResource(fileName, () -> createInputStream());
-	}
-
-	private InputStream createInputStream() {
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-		try {
-			outputStream.write(exportReport());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (JRException e) {
-			e.printStackTrace();
-		}
-		return new ByteArrayInputStream(outputStream.toByteArray());
-	}
-
-    public byte[] exportReport() throws JRException, FileNotFoundException, IOException {  
-        File tempFile = File.createTempFile("tmp", ".xls");
-        SimpleOutputStreamExporterOutput fileOut = new SimpleOutputStreamExporterOutput(tempFile);        
-        List<CarExcel> cars = getCars(getFilterdCars());
-        exportXlsReport(fileOut, cars);    
-        byte[] array = Files.readAllBytes(Paths.get(tempFile.getAbsolutePath()));
-        return array;
-    }
-
-	private List<CarExcel> getCars(List<Car> filterdCars) {
-		List<CarExcel> cars = new ArrayList<>();
-		for(Car car : filterdCars) {
-			CarExcel carExcel = new CarExcel(car);
-			cars.add(carExcel);
-		}
-		return cars;
-	}
-
-	private void exportXlsReport(SimpleOutputStreamExporterOutput fileOut, List<CarExcel> cars)
-			throws FileNotFoundException, JRException {
-		File file = ResourceUtils.getFile("classpath:cars.jrxml");
-        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(cars);
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("createdBy", "Cassiope");
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-        
-    	JRXlsExporter xlsExporter = new JRXlsExporter();
-        xlsExporter.setExporterInput(new SimpleExporterInput(jasperPrint));
-        xlsExporter.setExporterOutput(fileOut);
-        SimpleXlsReportConfiguration xlsReportConfiguration = new SimpleXlsReportConfiguration();
-        SimpleXlsExporterConfiguration xlsExporterConfiguration = new SimpleXlsExporterConfiguration();
-        xlsReportConfiguration.setOnePagePerSheet(true);
-        xlsReportConfiguration.setRemoveEmptySpaceBetweenRows(false);
-        xlsReportConfiguration.setDetectCellType(true);
-        xlsReportConfiguration.setWhitePageBackground(false);
-        xlsExporter.setConfiguration(xlsReportConfiguration);
-        xlsExporter.exportReport();
 	}
 
 	private Component createComponent(String mimeType, String fileName,
